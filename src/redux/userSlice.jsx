8b5d5c04cef7c02e5d9a3cc5e39405bd254f5b5f
Rajdev Yadav <ryindia2023@gmail.com>
2023-09-08 12:02:32 +0530

    files clean up and userSlice Created.

---
+++ src/redux/userSlice.jsx
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+        details: {
+            name: '',
+            phone: '',
+            email: '',
+            address: '',
+        },
+        cart: [],
+        history: [],
+        notification: 0,
+    },
+    reducers: {
+        // edit profile action 
+        editProfile: (state, action) => {
+            const { name, phone, email, address } = action.payload;
+            state.details.name = name;
+            state.details.email = email;
+            state.details.phone = phone;
+            state.details.address = address;
+        },
+        // add to cart action 
+        addToCart: (state, action) => {
+            state.cart.push[action.payload];
+            state.notification = + 1;
+        },
+        // delete from cart action 
+        deleteFromCart: (state, action) => {
+            const { id } = action.payload;
+            const deletedItem = state.cart.find(item => item.id == id);
+            if (deletedItem) {
+                state.notification = - 1;
+                return state.cart.filter(item => item.id != id);
+            }
+        }
+
+    }
+});
+
+export const {editProfile, addToCart, deleteFromCart} = userSlice.actions;
+export default userSlice.reducer ;
